e967800e9fbc25b64a967e65daf2cafc08a02c48
sakamoto0508s12 <sakamoto0508s12@gmail.com>
2025-12-11 23:29:16 +0900

    [update]

--- Assets/Mock/Scripts/Camera/LookOnCamera.cs
+++ Assets/Mock/Scripts/Camera/LookOnCamera.cs
@@ -1,16 +1,40 @@
+using Unity.Cinemachine;
 using UnityEngine;
 
 public class LookOnCamera : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public bool IsLookOn { get; private set; }
+    [SerializeField] private CinemachineCamera _camera;
+    [SerializeField] private CinemachineRotationComposer _composer;
+    [SerializeField] private Transform _playerPosition;
+    [SerializeField] private Transform _enemyPosition;
+    [SerializeField] private CameraConfig _cameraConfig;
+    private float _targetOffsetY;
+
+
+    [ContextMenu("LookOn")]
+    public void OnLockOn()
     {
-        
+        if (!IsLookOn)
+        {
+            IsLookOn = true;
+            _camera.Target.TrackingTarget = _enemyPosition;
+            _composer.TargetOffset.y = _cameraConfig.LookScreeEnemyY;
+        }
+        else
+        {
+            IsLookOn = false;
+            _camera.Target.TrackingTarget = _playerPosition;
+            _composer.TargetOffset.y= _cameraConfig.LookScreeEnemyY;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        if (_camera.Target.TrackingTarget == null)
+        {
+            _camera.Target.TrackingTarget = _playerPosition;
+        }
+        _composer.TargetOffset.y = _cameraConfig.LookScreenPlayerY;
     }
 }
--- Assets/Mock/Scripts/Config/CameraConfig.cs
+++ Assets/Mock/Scripts/Config/CameraConfig.cs
@@ -3,12 +3,12 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "CameraConfig", menuName = "ScriptableObjects/Camera/CameraConfig", order = 1)]
 public class CameraConfig : ScriptableObject
 {
-    public float LookScreenX => _lockScreenX;
-    public float LookScreenY => _lockScreenY;
+    public float LookScreenPlayerY => _lockScreenPlayerY;
+    public float LookScreeEnemyY => _lockScreenPlayerY;
     public float LerpSpeed => _lerpSpeed;
 
     [Header("LookON Settings")]
-    [SerializeField] private float _lockScreenX = 0.55f;
-    [SerializeField] private float _lockScreenY = 0.50f;
+    [SerializeField] private float _lockScreenPlayerY = 1.6f;
+    [SerializeField] private float _lookScreenEnemyY = 2.5f;
     [SerializeField] private float _lerpSpeed = 6.0f;
 }
