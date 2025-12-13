084547f52f0ce4445561f278b13f14dc5d6b245e
sakamoto0508s12 <sakamoto0508s12@gmail.com>
2025-12-13 18:16:16 +0900

    [update]caemra

--- Assets/Mock/Scripts/Camera/LookOnCamera.cs
+++ Assets/Mock/Scripts/Camera/LookOnCamera.cs
@@ -1,40 +1,38 @@
 using Unity.Cinemachine;
 using UnityEngine;
 
-public class LookOnCamera : MonoBehaviour
+public class LookOnCamera : MonoBehaviour, ICinemachineTargetGroup
 {
-    public bool IsLookOn { get; private set; }
-    [SerializeField] private CinemachineCamera _camera;
-    [SerializeField] private CinemachineRotationComposer _composer;
-    [SerializeField] private Transform _playerPosition;
-    [SerializeField] private Transform _enemyPosition;
-    [SerializeField] private CameraConfig _cameraConfig;
-    private float _targetOffsetY;
+    public Transform PlayerTarget => _playerTarget;
+    public Transform EnemyTarget => _enemyTarget;
 
+    [NoSaveDuringPlay]
+    [SerializeField] private Transform _playerTarget;
+    [NoSaveDuringPlay]
+    [SerializeField] private Transform _enemyTarget;
+    [SerializeField,Tooltip("各ターゲットの重み")]
+    private float _playerWeight = 1.0f;
+    [SerializeField] private float _enemyWeight = 1.0f;
+    [SerializeField, Tooltip("バウンディングボックスの余白")]
+    private float _boundsPodding = 1.0f;
 
-    [ContextMenu("LookOn")]
-    public void OnLockOn()
+    public bool IsValid => throw new System.NotImplementedException();
+
+    public Transform Transform => throw new System.NotImplementedException();
+
+    public Bounds BoundingBox => throw new System.NotImplementedException();
+
+    public BoundingSphere Sphere => throw new System.NotImplementedException();
+
+    public bool IsEmpty => throw new System.NotImplementedException();
+
+    public void GetViewSpaceAngularBounds(Matrix4x4 observer, out Vector2 minAngles, out Vector2 maxAngles, out Vector2 zRange)
     {
-        if (!IsLookOn)
-        {
-            IsLookOn = true;
-            _camera.Target.TrackingTarget = _enemyPosition;
-            _composer.TargetOffset.y = _cameraConfig.LookScreeEnemyY;
-        }
-        else
-        {
-            IsLookOn = false;
-            _camera.Target.TrackingTarget = _playerPosition;
-            _composer.TargetOffset.y= _cameraConfig.LookScreeEnemyY;
-        }
+        throw new System.NotImplementedException();
     }
 
-    private void Start()
+    public Bounds GetViewSpaceBoundingBox(Matrix4x4 observer, bool includeBehind)
     {
-        if (_camera.Target.TrackingTarget == null)
-        {
-            _camera.Target.TrackingTarget = _playerPosition;
-        }
-        _composer.TargetOffset.y = _cameraConfig.LookScreenPlayerY;
+        throw new System.NotImplementedException();
     }
 }
