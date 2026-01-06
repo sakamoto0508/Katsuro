18a6a74dfad9a3465f399ea0fd262cc8296f30c8
sakamoto0508s12 <sakamoto0508s12@gmail.com>
2026-01-06 16:46:13 +0900

    [update]camera

--- Assets/Mock/Scripts/Camera/LookOnCamera.cs
+++ Assets/Mock/Scripts/Camera/LookOnCamera.cs
@@ -34,12 +34,17 @@ public class LookOnCamera
         //プレイヤーの後ろにカメラを移動させる
         Vector3 desiredPos = _playerPosition.position - forward
             * _cameraConfig.CameraDistance + Vector3.up * _cameraConfig.CameraHeight;
-
+        //スムーズに移動させる
+        Transform camTransfrom= _camera.transform;
+        camTransfrom.position=Vector3.Lerp(camTransfrom.position, desiredPos
+            , Time.deltaTime * _cameraConfig.PositionSmooth);
     }
 
     public void LockOn()
     {
         IsLockOn = true;
+        // Cinemachine による制御を止める
+        _camera.enabled = false;
     }
 
     public void UnLockOn()
@@ -47,8 +52,14 @@ public class LookOnCamera
         if (!IsLockOn) return;
 
         IsLockOn = false;
+        // Cinemachine を戻す
+        _camera.enabled = true;
     }
 
+    /// <summary>
+    /// プレイヤー用：敵を見る方向（移動・回転用）
+    /// </summary>
+    /// <returns></returns>
     public Vector3 ReturnLockOnDirection()
     {
         if (!IsLockOn) return Vector3.zero;
