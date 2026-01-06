d9da19c2e727d44d9263e8e71671ce7cda02cee7
sakamoto0508s12 <sakamoto0508s12@gmail.com>
2026-01-06 18:34:43 +0900

    [update]cameramanager

--- Assets/Mock/Scripts/Camera/CameraManager.cs
+++ Assets/Mock/Scripts/Camera/CameraManager.cs
@@ -2,5 +2,9 @@ using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
-    
+    private LockOnCameraMover _lockOnCamera;
+    public void Init()
+    {
+        _lockOnCamera = new LockOnCameraMover();
+    }
 }
