1f67bb8fec3fd993cc3d325b6b4c97546c3816b5
sakamoto0508s12 <sakamoto0508s12@gmail.com>
2026-01-06 17:26:16 +0900

    [update]cameraconfig

--- Assets/Mock/Scripts/Camera/LookOnCamera.cs
+++ Assets/Mock/Scripts/Camera/LookOnCamera.cs
@@ -22,29 +22,7 @@ public class LookOnCamera
     public void Update()
     {
         if (!IsLockOn) return;
-        //敵の方向ベクトルを求める。メソッドを使わないのはy成分を0にしているため。
-        Vector3 toEnemy = _enemyPosition.position - _playerPosition.position;
-        toEnemy.y = 0;
-        if (toEnemy.sqrMagnitude < 0.01f)
-        {
-            UnLockOn();
-            return;
-        }
-        Vector3 forward = toEnemy.normalized;
-        //プレイヤーの後ろにカメラを移動させる。
-        Vector3 desiredPos = _playerPosition.position - forward
-            * _cameraConfig.CameraDistance + Vector3.up * _cameraConfig.CameraHeight;
-        //スムーズに移動させる。
-        Transform camTransfrom = _camera.transform;
-        camTransfrom.position = Vector3.Lerp(camTransfrom.position, desiredPos
-            , Time.deltaTime * _cameraConfig.PositionSmooth);
-        //カメラを敵に向ける。
-        Vector3 lookDir = (_enemyPosition.position + Vector3.up * _cameraConfig.LookAtHeight)
-            - camTransfrom.position;
-        //スムーズに回転させる。
-        Quaternion targetRot =Quaternion.LookRotation(lookDir.normalized);
-        camTransfrom.rotation = Quaternion.Slerp(camTransfrom.rotation, targetRot
-            , Time.deltaTime * _cameraConfig.RotationSmooth);
+        UpdateLockOnCamera();
     }
 
     public void LockOn()
@@ -74,5 +52,32 @@ public class LookOnCamera
         Vector3 direction = _enemyPosition.position - _playerPosition.position;
         direction.y = 0;
         return direction.normalized;
+    }
+
+    private void UpdateLockOnCamera()
+    {
+        //敵の方向ベクトルを求める。メソッドを使わないのはy成分を0にしているため。
+        Vector3 toEnemy = _enemyPosition.position - _playerPosition.position;
+        toEnemy.y = 0;
+        if (toEnemy.sqrMagnitude < 0.01f)
+        {
+            UnLockOn();
+            return;
+        }
+        Vector3 forward = toEnemy.normalized;
+        //プレイヤーの後ろにカメラを移動させる。
+        Vector3 desiredPos = _playerPosition.position - forward
+            * _cameraConfig.CameraDistance + Vector3.up * _cameraConfig.CameraHeight;
+        //スムーズに移動させる。
+        Transform camTransfrom = _camera.transform;
+        camTransfrom.position = Vector3.Lerp(camTransfrom.position, desiredPos
+            , Time.deltaTime * _cameraConfig.PositionSmooth);
+        //カメラを敵に向ける。
+        Vector3 lookDir = (_enemyPosition.position + Vector3.up * _cameraConfig.LookAtHeight)
+            - camTransfrom.position;
+        //スムーズに回転させる。
+        Quaternion targetRot = Quaternion.LookRotation(lookDir.normalized);
+        camTransfrom.rotation = Quaternion.Slerp(camTransfrom.rotation, targetRot
+            , Time.deltaTime * _cameraConfig.RotationSmooth);
     }
 }
--- Assets/Mock/Scripts/Config/CameraConfig.cs
+++ Assets/Mock/Scripts/Config/CameraConfig.cs
@@ -3,16 +3,12 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "CameraConfig", menuName = "ScriptableObjects/Camera/CameraConfig", order = 1)]
 public class CameraConfig : ScriptableObject
 {
-    public Transform PlayerPosition => _playerPosition;
-    public Transform EnemyPosition => _enemyPosition;
     public float CameraDistance => _cameraDistance;
     public float CameraHeight => _cameraHeight;
     public float PositionSmooth => _positionSmooth;
     public float RotationSmooth => _rotationSmooth;
     public float LookAtHeight => _lookAtHeight;
 
-    [SerializeField] private Transform _playerPosition;
-    [SerializeField] protected Transform _enemyPosition;
     [SerializeField] private float _cameraDistance = 4.5f;
     [SerializeField] private float _cameraHeight = 2.0f;
     [SerializeField] private float _positionSmooth = 10f;
