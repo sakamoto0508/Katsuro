ba16f98db21667be03850aea717ccfb4cb62ae53
sakamoto0508s12 <sakamoto0508s12@gmail.com>
2026-01-21 17:02:28 +0900

    [update]playerattack

--- Assets/Mock/Scripts/Player/PlayerAttacker.cs
+++ Assets/Mock/Scripts/Player/PlayerAttacker.cs
@@ -2,25 +2,53 @@ using UnityEngine;
 
 public class PlayerAttacker
 {
-    public PlayerAttacker(PlayerAnimationController animController, AnimationName animName)
+    public PlayerAttacker(PlayerAnimationController animController, AnimationName animName, PlayerWeapon plaerWeapon)
     {
         _animController = animController;
         _animName = animName;
+        _weapon = plaerWeapon;
     }
+
     public bool IsDrawingSword { get; private set; } = false;
     private PlayerAnimationController _animController;
     private AnimationName _animName;
+    private PlayerWeapon _weapon;
 
     public void DrawSword()
     {
         if (IsDrawingSword) return;
 
         IsDrawingSword = true;
-        _animController?.PlayTrriger(_animName.IsDrawingSword);
+        if (!string.IsNullOrEmpty(_animName?.IsDrawingSword))
+            _animController?.PlayTrriger(_animName.IsDrawingSword);
     }
 
-    private void PlayAttack(string triggerName,bool enableWeaponHitbox)
-    {
+    public void PlayLightAttack(bool enableWeaponHitbox)
+        => PlayAttack(_animName?.LightAttack, enableWeaponHitbox);
+    public void PlayStrongAttack(bool enableWeaponHitbox)
+        => PlayAttack(_animName?.StrongAttack, enableWeaponHitbox);
+    public void PlayJustAvoidAttack(bool enableWeaponHitbox)
+        => PlayAttack(_animName?.JustAvoidAttack, enableWeaponHitbox);
 
+    public void EndAttack()
+    {
+        _weapon?.DisableHitbox();
+    }
+
+    private void PlayAttack(string triggerName, bool enableWeaponHitbox)
+    {
+        if (!string.IsNullOrEmpty(triggerName))
+        {
+            _animController?.PlayTrriger(triggerName);
+        }
+
+        if (enableWeaponHitbox)
+        {
+            _weapon?.EnableHitbox();
+        }
+        else
+        {
+            _weapon?.DisableHitbox();
+        }
     }
 }
