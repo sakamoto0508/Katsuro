e719670b7008694ddb3c6399371e3bc0beace048
sakamoto0508s12 <sakamoto0508s12@gmail.com>
2026-01-26 02:53:37 +0900

    [update]config

--- Assets/Mock/Scripts/Config/PlayerPassiveBuffSet.cs
+++ Assets/Mock/Scripts/Config/PlayerPassiveBuffSet.cs
@@ -1,16 +1,25 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerPassiveBuffSet : MonoBehaviour
+[CreateAssetMenu(fileName = "PlayerPassiveBuffSet"
+    , menuName = "ScriptableObjects/Player/PlayerPassiveBuffSet")]
+public class PlayerPassiveBuffSet : ScriptableObject
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public IReadOnlyList<PassiveBuffEntry> Buffs => _buffs; 
+    [SerializeField] private List<PassiveBuffEntry> _buffs = new();
+}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+[Serializable]
+public sealed class PassiveBuffEntry
+{
+    public string Label => _label;
+    public float AttackPowerMultiplier => _attackPowerMultiplier;
+    public float FlatAttackBonus => _flatAttackBonus;
+    public GameObject OnHitEffectPrefab => _onHitEffectPrefab;
+    [SerializeField] private string _label = "PassiveBuff";
+    [SerializeField, Min(0f)] private float _attackPowerMultiplier = 1f;
+    [SerializeField] private float _flatAttackBonus;
+    [SerializeField] private GameObject _onHitEffectPrefab;
+
 }
