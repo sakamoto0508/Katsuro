9aa4a3d335a56892c1796852e8440635ad9fe32b
sakamoto0508s12 <sakamoto0508s12@gmail.com>
2026-02-02 16:27:32 +0900

    [update]enemystate

--- Assets/Mock/Scripts/Enemy/EnemyState/EnemyState.cs
+++ Assets/Mock/Scripts/Enemy/EnemyState/EnemyState.cs
@@ -1,16 +1,8 @@
-using UnityEngine;
-
-public class EnemyState : MonoBehaviour
+public enum EnemyState
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    Idle,
+    Attack,
+    Recovery,
+    Stagger,
+    Dead,
 }
